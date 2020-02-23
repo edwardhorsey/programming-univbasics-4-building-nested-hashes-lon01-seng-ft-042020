ebaae0c55618b9559657ec956d34a022accee905
Edward Horsey <edwardhorsey@me.com>
2020-02-23 00:14:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,9 +13,7 @@ def monopoly_with_second_tier
   # complexity.
   
 	{
-	  :railroads => {:pieces => {
-	                                  }
-	                     }
+	  :railroads => {:pieces => 4}
 	  }
 end
 
